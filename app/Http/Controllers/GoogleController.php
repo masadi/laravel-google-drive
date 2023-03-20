bc6fb22de73b31cb33dde1d5a80a63051a117fc2
masadi <masadi.com@gmail.com>
2023-03-21 06:52:46 +0700

    Update GoogleController.php

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -8,10 +8,19 @@ use Illuminate\Support\Facades\Http;
 class GoogleController extends Controller
 {
     public function index(){
+        $path = request()->attachment->store(request()->generated_new_name);
+        //return $path;
         $data = [
             'request' => request()->all(),
             'attachment' => request()->attachment,
+            'path' => $path,
         ];
         return response()->json($data);
+    }
+    private function create_folder($folder){
+        return Storage::cloud()->makeDirectory($folder);
+    }
+    private function upload_file($folder, $file){
+        return Storage::cloud()->makeDirectory($folder.'/'.$file);
     }
 }
