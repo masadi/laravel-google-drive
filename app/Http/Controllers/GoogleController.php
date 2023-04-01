37a58d0f67e6b00231097910b89c754e825ba647
masadi <masadi.com@gmail.com>
2023-04-01 23:26:23 +0700

    Update GoogleController.php

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -25,10 +25,9 @@ class GoogleController extends Controller
     }
     public function get(){
         $filename = request()->filename;
-        $data = ['filename' => $filename];
-        return response()->json($data);
         $rawData = Storage::cloud()->get($filename); // raw content
         $file = Storage::cloud()->getAdapter()->getMetadata($filename); // array with file info
+        return response()->json($file);
         return response($rawData, 200)
             ->header('ContentType', $file['mimetype'])
             ->header('Content-Disposition', "attachment; filename=$filename");
